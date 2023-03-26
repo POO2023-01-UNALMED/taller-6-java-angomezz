cedb375236cc99207ed943b362077481999f69dc
angomezz <angomezz@unal.edu.co>
2023-03-25 23:27:58 -0500

    Segundo intento

--- src/main/java/vehiculo/Fabricante.java
+++ src/main/java/vehiculo/Fabricante.java
@@ -1,6 +1,19 @@
 package vehiculo;
+import java.util.ArrayList;
 
 public class Fabricante {
     private String nombre;
     private String pais;
-}
+    private int canVehiculos;
+    private static ArrayList<Fabricante> listaDeFabricantes = new ArrayList<Fabricante>();
+
+
+    public Fabricante(String nombre, Pais pais);
+    this.nombre = nombre;
+    this.pais = pais;
+
+
+
+
+
+
--- src/main/java/vehiculo/Pais.java
+++ src/main/java/vehiculo/Pais.java
@@ -1,6 +1,42 @@
 package vehiculo;
+import java.util.ArrayList;
 
 public class Pais {
     private String nombre;
+    private int canVehiculos;
+    private static ArrayList<Pais> listaDePaises = new Arraylist<Pais>();
 
-}
+    public Pais(String nombre){
+        this.nombre = nombre;
+        listaDePaises.add(this);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCanVehiculos() {
+        return canVehiculos;
+    }
+    public void setCanVehiculos(int v) {
+        canVehiculos = v;
+    }
+
+    public static Pais paisMasVendedor(){
+       Pais masVendedor = null;
+       int primero = 0;
+       for(int i = 0; i < listaDePaises.size();i++){
+           if (listaDePaises.get(i).getCanVehiculos() > primero){
+               masVendedor = listaDePaises.get(i);
+               primero = listaDePaises.get(i).getCanVehiculos();
+           }
+       }
+       return masVendedor;
+       }
+
+
+
+    }
